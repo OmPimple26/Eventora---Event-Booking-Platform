43c785119c3a2e70060c68aa96d7ca73c7b8d163
OmPimple26 <ompimple04@gmail.com>
2026-08-31 20:55:15 +0530

    Added Home, Login and Register Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,31 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 
-import './App.css'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <Router>
+        <div className="min-h-screen bg-gray-50 flex flex-col">
+          <Navbar />
+          <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
